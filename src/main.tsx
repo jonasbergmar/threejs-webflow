b207e3370ad8adfd62e52aede6cf90ec407f3e45
jonasbergmar <jonas.bergmar@hotmail.com>
2026-01-05 16:58:21 +0100

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -157,10 +157,10 @@ function initAboutModel() {
   renderer.setSize(container.clientWidth, container.clientHeight);
   container.appendChild(renderer.domElement);
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1.2);
   scene.add(ambientLight);
 
-  const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+  const dirLight = new THREE.DirectionalLight(0xffffff, 2.5);
   dirLight.position.set(5, 5, 5);
   scene.add(dirLight);
   const modelUrl =
