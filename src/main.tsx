6e773cf63a69616a6e78665cec53d10fec0c0a8c
jonasbergmar <jonas.bergmar@hotmail.com>
2026-01-06 16:24:52 +0100

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -163,7 +163,7 @@ function initAboutModel() {
     0.1,
     100
   );
-  camera.position.set(0, 0, 3);
+  camera.position.set(0, 0, 4);
 
   const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setClearColor(0x000000, 0);
