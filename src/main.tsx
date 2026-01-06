713ddd176c652d52af8eb4c35043cc73c0113e27
jonasbergmar <jonas.bergmar@hotmail.com>
2026-01-06 16:28:08 +0100

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -188,7 +188,7 @@ function initAboutModel() {
       const model = gltf.scene;
       scene.add(model);
       model.position.y = -0.5;
-      model.scale.set(0.5, 0.5, 0.5);
+      model.scale.set(0.3, 0.3, 0.3);
       function animateModel() {
         const mouseTargetX = mouse.y * Math.PI * 0.25;
         const mouseTargetY = mouse.x * Math.PI * 0.25;
