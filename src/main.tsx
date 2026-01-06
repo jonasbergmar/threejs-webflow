95a40f7aff5976aa986edba55a1aa2ff518ba98b
jonasbergmar <jonas.bergmar@hotmail.com>
2026-01-06 11:11:40 +0100

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -173,7 +173,7 @@ function initAboutModel() {
   controls.enableDamping = true;
   controls.enableZoom = false; // Optional, disable if you want consistent scale
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 1.2);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 2);
   scene.add(ambientLight);
 
   const dirLight = new THREE.DirectionalLight(0xffffff, 2.5);
